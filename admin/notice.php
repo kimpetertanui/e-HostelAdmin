1df15cf06f1e9b05d567f91bd29566ea229d98a0
kimpetertanui <kimelipeter526@gmail.com>
2019-10-02 09:41:47 +0300

    modified the style

--- admin/notice.php
+++ admin/notice.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>e-Hostel Admin - Dashboard ::Notice</title>
+  <title>e-Hostel::Notice</title>
 
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
